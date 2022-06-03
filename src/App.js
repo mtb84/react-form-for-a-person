9f8e0b7a99a4f2648d5226994f5024271ace6468
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-03 13:07:57 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import React from 'react';
+
 import './App.css';
 
 
@@ -8,19 +10,20 @@ function App() {
   const [lastName, setLastName] = useState("");
   const [address, setAddress] = useState("")
   const [phoneNumber, setPhoneNumber] = useState("")
-  const [posts, setPosts] = useState([])
+  const [contacts, setContacts] = useState([])
 
 
-  const addPost = (e) => {
-    const newPost = {
+  const addContacts = (e) => {
+    e.preventDefault();
+    const newContacts = {
       firstName,
       lastName,
       address,
       phoneNumber
     }
- 
 
-    setPosts([...posts, newPost]);
+ 
+    setContacts([...contacts, newContacts]);
     setFirstName("");
     setLastName("");
     setAddress("");
@@ -42,32 +45,54 @@ function App() {
     setPhoneNumber(e.target.value);
   }
 
+  const contactsList = contacts.map((contact) => (
+
+    <li key={contact.id}>
+        <div className="contact-info">
+            <ul>
+                <li>{contact.firstName}</li>
+                <li>{contact.lastName}</li>
+                <li>{contact.address}</li>
+                <li>{contact.phoneNumber}</li>
+            </ul>
+        </div>
+    </li>
+  ));
+
+
   
 
     return (
 
     <div className="App">
-   
-    <form onSubmit={addPost}>
-    <h2>Contact Information</h2>
-    <label htmlFor=""> First Name:
-    <input value={firstName} onChange={updateFirstName} type="text" name='firstName'/>
-    </label> <br />
-    <label htmlFor=""> Last Name:
-    <input value={lastName} onChange={updateLastName} type="text" name="lastName"/>
-    </label> <br />
-    <label htmlFor=""> Address:
-    <input type="text"  value={address} onChange={updateAddress} name="address" />
-    </label> <br />
-    <label> Phone Number:
-    <input type="text" value={phoneNumber} onChange={updatePhoneNumber} name="phoneNumber" />
-    </label><br />
-    <button  type="submit">Save</button>
-    </form>
+      
+      <form onSubmit={addContacts}>
+        <h2>Contact Information</h2>
+        <label htmlFor=""> First Name:
+        <input value={firstName} onChange={updateFirstName} type="text" name='firstName'/>
+        </label> <br />
+        <label htmlFor=""> Last Name:
+        <input value={lastName} onChange={updateLastName} type="text" name="lastName"/>
+        </label> <br />
+        <label htmlFor=""> Address:
+        <input type="text"  value={address} onChange={updateAddress} name="address" />
+        </label> <br />
+        <label> Phone Number:
+        <input type="text" value={phoneNumber} onChange={updatePhoneNumber} name="phoneNumber" />
+        </label><br />
+        <button type="submit">Save</button>
+      </form>
+
+      <ol className='contacts'>
+        {contactsList}
+      </ol>
+
+
     </div>
 
   
   );
+  
   
 }
 export default App;
